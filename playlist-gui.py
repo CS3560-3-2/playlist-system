e3e3c23be603badcf407a7600c29b7c5b5de1030
Brandon Vo <90083597+brdnvo@users.noreply.github.com>
2024-04-23 23:49:12 -0700

    playlist screen

--- playlist-gui.py
+++ playlist-gui.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from PIL import ImageTk, Image
 
 def test():
   print('insert songs here')
@@ -8,13 +9,14 @@ window = tk.Tk()
 window.title('Your Playlist')
 window.geometry('1600x900')
 
+
 table = ttk.Treeview(window, columns = ('title', 'artist', 'duration'), show = 'headings')
 table.heading('title', text = 'Song Title')
 table.heading('artist', text = 'Artist')
 table.heading('duration', text = 'Duration')
 table.pack(fill = 'both', expand = True)
 
-title_label = ttk.Label(master = window, text = 'test', font = 'Arial 18')
+title_label = ttk.Label(master = window, font = 'Arial 18')
 title_label.pack()
 
 # Insert songs here
@@ -32,7 +34,8 @@ def delete_song(_):
   for i in table.selection():
     table.delete(i)
 
-button = tk.Button(window, text = "Play", command = test)
+play_icon = ImageTk.PhotoImage(Image.open("play.png"))
+button = tk.Button(window, image = play_icon, command = test)
 button.pack()
 
 window.mainloop()
